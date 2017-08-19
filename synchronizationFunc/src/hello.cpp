b2a0620cd8b5051a94a8f7659614a7316f244b52
Acceptedlc <Accepted@icloud.com>
2017-08-19 16:25:53 +0800

    finish first demo

---
+++ synchronizationFunc/src/hello.cpp
@@ -0,0 +1,12 @@
+#include <node.h>
+
+void hello(const v8::FunctionCallbackInfo<v8::Value>& args){
+    v8::Isolate* isolate = args.GetIsolate();
+    args.GetReturnValue().Set(v8::String::NewFromUtf8(isolate, "world"));
+}
+
+void init(v8::Local<v8::Object> exports) {
+    NODE_SET_METHOD(exports, "hello", hello);
+}
+
+NODE_MODULE(addon, init)
